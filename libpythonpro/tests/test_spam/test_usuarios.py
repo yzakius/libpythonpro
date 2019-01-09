603faca505bcd5be0ef89cf39f6f7f0de43786fd
yzakius <isaac@yzakius.me>
2019-01-09 11:20:16 -0300

    Implementado o teste de usuarios com o banco de dados.
    Close #25

---
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -0,0 +1,25 @@
+from libpythonpro.spam.db import Conexao
+from libpythonpro.spam.modelos import Usuario
+
+
+def test_salvar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuario = Usuario(nome='Isaac')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
+
+
+def test_listar_usuarios():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuarios = [Usuario(nome='Isaac'), Usuario(nome='Manolo')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
